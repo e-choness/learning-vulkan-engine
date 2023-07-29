e0eb8870d6bb41317d956b7ddbcc6699ea4f2d13
Echo Yin <echo_ybl@hotmail.com>
2023-07-29 16:40:14 -0400

    Add vulkan initial setups

--- src/include/vk_engine.h
+++ src/include/vk_engine.h
@@ -3,6 +3,7 @@
 //
 #pragma once
 #include "vk_types.h"
+#include <vector>
 
 class VulkanEngine{
 public:
@@ -12,6 +13,17 @@ public:
     VkExtent2D _windowExtent{1600, 900};
     struct SDL_Window* _window{ nullptr };
 
+    VkInstance _instance;
+    VkDebugUtilsMessengerEXT _debug_messager;
+    VkPhysicalDevice _chosenGPU;
+    VkDevice _device;
+    VkSurfaceKHR _surface;
+
+    VkSwapchainKHR _swapchain;
+    VkFormat _swapchainImageFormat;
+    std::vector<VkImage> _swapchainImages;
+    std::vector<VkImageView> _swapchainImageViews;
+
     void init();
 
     void cleanup() const;
@@ -19,4 +31,8 @@ public:
     void draw();
 
     void run();
+
+private:
+    void init_vulkan();
+    void init_swapchain();
 };
--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -2,11 +2,27 @@
 // Created by echo_ on 2023-07-28.
 //
 #include "vk_engine.h"
+#include "vk_types.h"
+#include "vk_init.h"
+#include "include/vk_engine.h"
+
 
 #include <SDL.h>
 #include <SDL_vulkan.h>
-#include <vk_types.h>
+#include <VkBootstrap.h>
+#include <iostream>
 
+#define VK_CHECK(x)                 \
+    do                              \
+    {                               \
+        VkResult err = x;           \
+        if (err)                    \
+        {                           \
+            std::cout << "Detected Vulkan error: " << err << "\n"; \
+            abort();                \
+        }                           \
+    } while(0)                      \
+}
 
 void VulkanEngine::init() {
     SDL_Init(SDL_INIT_VIDEO);
@@ -18,10 +34,26 @@ void VulkanEngine::init() {
             _windowExtent.height,
             window_flags
             );
+
+    init_vulkan();
+
+    init_swapchain();
+
+    _isInitialized = true;
 }
 
 void VulkanEngine::cleanup() const {
     if(_isInitialized){
+        vkDestroySwapchainKHR(_device, _swapchain, nullptr);
+
+        for(int i=0; i<_swapchainImageViews.size(); i++){
+            vkDestroyImageView(_device, _swapchainImageViews[i], nullptr);
+        }
+
+        vkDestroyDevice(_device, nullptr);
+        vkDestroySurfaceKHR(_instance, _surface, nullptr);
+        vkb::destroy_debug_utils_messenger(_instance, _debug_messager);
+        vkDestroyInstance(_instance, nullptr);
         SDL_DestroyWindow(_window);
     }
 }
@@ -32,12 +64,64 @@ void VulkanEngine::draw() {
 
 void VulkanEngine::run() {
     SDL_Event event;
-    bool bQuit = false;
-    while(bQuit){
+    bool shouldQuit = false;
+    while(!shouldQuit){
         while(SDL_PollEvent(&event) != 0){
-            if(event.type == SDL_EVENT_QUIT)
-                bQuit = true;
+            if(event.type == SDL_EVENT_QUIT )
+                shouldQuit = true;
         }
         draw();
     }
+}
+
+void VulkanEngine::init_vulkan() {
+    vkb::InstanceBuilder builder;
+
+    auto inst_rec = builder.set_app_name("Example Vulkan Application")
+            .request_validation_layers(true)
+            .require_api_version(1,1,0)
+            .use_default_debug_messenger()
+            .build();
+
+    vkb::Instance vkb_inst = inst_rec.value();
+
+    _instance = vkb_inst.instance;
+
+    _debug_messager = vkb_inst.debug_messenger;
+
+    // Device
+    SDL_Vulkan_CreateSurface(_window, _instance, &_surface);
+
+    vkb::PhysicalDeviceSelector selector{ vkb_inst };
+
+    vkb::PhysicalDevice physicalDevice = selector
+            .set_minimum_version(1,1)
+            .set_surface(_surface)
+            .select()
+            .value();
+
+    vkb::DeviceBuilder deviceBuilder{ physicalDevice};
+
+    vkb::Device vkbDevice = deviceBuilder.build().value();
+
+    _device = vkbDevice.device;
+    _chosenGPU = physicalDevice.physical_device;
+
+}
+
+void VulkanEngine::init_swapchain() {
+    vkb::SwapchainBuilder swapchainBuilder {_chosenGPU, _device, _surface};
+
+    vkb::Swapchain vkbSwapchain = swapchainBuilder
+            .use_default_format_selection()
+            .set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+            .set_desired_extent(_windowExtent.width, _windowExtent.height)
+            .build()
+            .value();
+
+    _swapchain = vkbSwapchain.swapchain;
+    _swapchainImages = vkbSwapchain.get_images().value();
+    _swapchainImageViews = vkbSwapchain.get_image_views().value();
+
+    _swapchainImageFormat = vkbSwapchain.image_format;
 }
