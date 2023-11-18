475966e8f61e1d9cb14574d23d41e1174bba9a4b
Echo Yin <echo_ybl@hotmail.com>
2023-11-18 14:31:33 -0500

    update and experiment with the glfw framework

--- src/include/EngineWindow.h
+++ src/include/EngineWindow.h
@@ -2,39 +2,36 @@
 
 #define GLFW_INCLUDE_NONE
 
-#include "EngineExtent.h"
 #include <GLFW/glfw3.h>
 
 namespace engine{
+
+    struct WindowSize{
+        uint16_t Width;
+        uint16_t Height;
+    };
+
     struct WindowProperties{
-        Extent2D Size = {1600, 900};
-        const char* WindowTitle = {"Vulkan Engine"};
-        GLFWmonitor* WindowMonitor = {nullptr};
-        GLFWwindow* WindowShare = {nullptr};
+        WindowSize WindowSize ;
+        const char* WindowTitle;
+        GLFWmonitor* WindowMonitor;
+        GLFWwindow* WindowShare;
     };
 
     class EngineWindow{
     public:
-        explicit EngineWindow(WindowProperties& windowProperties);
+        // Engine window basic functions
+        EngineWindow()= default;
         ~EngineWindow() = default;
-
-        // Prevent window self copying, shared glfw window will be terminated through deconstruction
-        EngineWindow(const EngineWindow &) = delete;
-        EngineWindow &operator=(const EngineWindow &) = delete;
-
-        void SetProperties(WindowProperties& windowProperties);
-        bool InitWindow();
+        bool InitWindow(WindowProperties& winProperties);
         void Run();
         void CleanUpWindow();
+
+        // Additional functions
         bool ShouldClose();
-        bool IsInitialized();
-        double GetTime();
-        void SetTitle(const char* title);
-        GLFWwindow* GetWindowInstance() {return m_WindowInstance;}
 
     private:
-        WindowProperties m_WinProperties;
-        GLFWwindow* m_WindowInstance;
-        bool m_IsInitialized;
+        WindowProperties mWinProperties{};
+        GLFWwindow* mGlfwWindow{};
     };
 }
