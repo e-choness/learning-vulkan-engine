45e98c0302073cdcbd283e7be1285319a46603dc
Echo Yin <echo_ybl@hotmail.com>
2023-11-18 22:30:29 -0500

    Show cursor position when dragging mouse

--- src/EngineWindow.cpp
+++ src/EngineWindow.cpp
@@ -36,16 +36,28 @@ namespace engine{
                 int xPos, yPos, width, height;
                 glfwGetMonitorWorkarea(glfwGetPrimaryMonitor(), &xPos, &yPos, &width, &height);
                 glfwSetWindowPos(window, xPos, yPos);
-
                 glfwSetWindowSize(window, width, height);
+                glfwFocusWindow(window);
 
                 std::cout << "Engine goes full screen mode.\n";
             }
 
+            // Press A to maximize window mode
+            if(key == GLFW_KEY_A && action == GLFW_PRESS){
+                glfwMaximizeWindow(window);
+                glfwFocusWindow(window);
+                std::cout << "Engine goes maximized window mode.\n";
+            }
+
             // Press W back to window mode
             if(key == GLFW_KEY_W && action == GLFW_PRESS){
+                // A bug when the first time return to window mode, it looses focus and stays at maximized window
+                // Set focus window first would fix maximizing mode, but still loosing focus when returning to window mode
+                glfwFocusWindow(window);
+
                 glfwSetWindowPos(window,WINDOW_X_OFFSET, WINDOW_Y_OFFSET);
                 glfwSetWindowSize(window, DEFAULT_WIDTH, DEFAULT_HEIGHT);
+
                 std::cout << "Engine is back to window mode.\n";
             }
 
@@ -58,7 +70,7 @@ namespace engine{
         void WindowFocusCallback(GLFWwindow* window, int focused){
             // TODO: have fun with when the window is focused and the alternative
             if(focused == GLFW_TRUE){
-                glfwFocusWindow(window);
+
                 glfwSetWindowOpacity(window, WINDOW_SOLID);
                 glfwSetWindowTitle(window, "The One and Only Engine.");
                 std::cout << "Engine window focused.\n";
@@ -76,10 +88,16 @@ namespace engine{
         }
 
         void WindowCursorPositionCallback(GLFWwindow* window, double x, double y){
-//            // TODO: Keep track of cursor position as input
-            glfwSetCursorPos(window, x, y);
-            std::cout << "Cursor position: (" << x << "," << y << ")\n";
+            // TODO: show cursor position when left mouse is clicked
+            // Left mouse click to show cursor position
+            // Dragging mouse click will update the position in the next update?
+            auto key = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
+            if(key == GLFW_PRESS){
+                std::cout << "At engine runtime " << glfwGetTime() << "s, Cursor position: (" << x << "," << y << ")\n";
+            }
         }
+
+
 
         void WindowIconifyCallback(GLFWwindow* window, int iconified){
             if(iconified == GLFW_TRUE){
@@ -134,7 +152,9 @@ namespace engine{
     }
 
     void EngineWindow::Run(){
-//        glfwSwapBuffers(mGlfwWindow); // Swap buffers should have OpenGL or OpenGL ES context, it's not setup yet.
+        // Swap buffers should have OpenGL or OpenGL ES context, it's not setup yet.
+//        glfwSwapBuffers(mGlfwWindow);
+
         glfwPollEvents();
     }
 
